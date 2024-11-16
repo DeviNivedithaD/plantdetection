c9638ec0a02d23fabe956e35eb61e530007c418a
DeviNivedithaD <159867854+DeviNivedithaD@users.noreply.github.com>
2024-11-16 23:35:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ app_mode = st.sidebar.selectbox("Select Page", ["Home", "About", "Disease Recogn
 if (app_mode == "Home"):
     st.header("PLANT DISEASE RECOGNITION SYSTEM")
     image_path = "th.jpg"
-    st.image(image_path, use_column_width=True)
+    st.image(image_path, use_container_width=True)
     st.markdown("""
     Welcome to the Plant Disease Recognition System! 🌿🔍
     
@@ -104,7 +104,7 @@ elif (app_mode == "Disease Recognition"):
     test_image = st.file_uploader("Choose an Image:")
     if (test_image is not None):
         if (st.button("Show Image")):
-            st.image(test_image, width=400, use_column_width=True)
+            st.image(test_image, width=400, use_container_width=True)
         # Predict button
         if (st.button("Predict")):
             st.snow()
