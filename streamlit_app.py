438aa24804524a2b261f540c1abe70419ea58433
DeviNivedithaD <159867854+DeviNivedithaD@users.noreply.github.com>
2024-12-11 01:20:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ app_mode = st.sidebar.selectbox("Select Page", ["Home", "About", "Disease Recogn
 
 # Main Page
 if (app_mode == "Home"):
-st.header("PLANT DISEASE RECOGNITION SYSTEM")
+    st.header("PLANT DISEASE RECOGNITION SYSTEM")
     image_path = "th.jpg"
     st.image(image_path, use_container_width=True)
     st.markdown("""
