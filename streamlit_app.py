85767608f98133820b34af808df3240dc49b2270
DeviNivedithaD <159867854+DeviNivedithaD@users.noreply.github.com>
2024-12-10 16:37:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,7 +145,7 @@ elif app_mode == "Disease Recognition":
                 st.write("Processing...")
                 result_index, confidence = model_prediction("temp_image.jpg")
                 confidence_threshold = 0.7
-                  class_name = ['Apple__Apple_scab', 'Apple_Black_rot', 'Apple_Cedar_apple_rust', 'Apple__healthy',
+                class_name = ['Apple__Apple_scab', 'Apple_Black_rot', 'Apple_Cedar_apple_rust', 'Apple__healthy',
                           'Blueberry__healthy', 'Cherry_(including_sour)__Powdery_mildew',
                           'Cherry_(including_sour)__healthy', 'Corn_(maize)__Cercospora_leaf_spot Gray_leaf_spot',
                           'Corn_(maize)__Common_rust_', 'Corn_(maize)__Northern_Leaf_Blight', 'Corn_(maize)__healthy',
